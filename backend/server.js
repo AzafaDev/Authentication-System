e8b8b417e479917a21a5f2a1f450a1f5c8d4bfd9
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-03-17 10:38:44 +0700

    project completed

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ import { connectDb } from "./configs/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(
@@ -24,15 +23,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+connectDb();
 
-connectDb().then(() => {
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
-});
+}
+
+export default app;
